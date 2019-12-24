04f77caf1d4258d46648d8efbe198b93fabf104a
放心家 <sunjian.shanghai@foxmail.com>
2019-12-24 13:38:26 +0800

    增加安装apk的功能

--- android/src/main/java/com/reactlibrary/RNUpdateModule.java
+++ android/src/main/java/com/reactlibrary/RNUpdateModule.java
@@ -4,15 +4,21 @@ package com.reactlibrary.update;
 import android.app.Activity;
 import android.os.Handler;
 import android.os.Looper;
+import android.net.Uri;
+import android.os.Build;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.support.v4.content.FileProvider;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.JSBundleLoader;
 
 import java.io.File;
@@ -77,6 +83,60 @@ public class RNUpdateModule extends ReactContextBaseJavaModule {
     }
     return "";
   }
+
+  @ReactMethod
+    public void actionViewIntent(String path, String mime, final Promise promise) {
+        try {
+            Uri uriForFile = FileProvider.getUriForFile(getCurrentActivity(),
+                    this.getReactApplicationContext().getPackageName() + ".provider", new File(path));
+
+            if (Build.VERSION.SDK_INT >= 24) {
+                // Create the intent with data and type
+                Intent intent = new Intent(Intent.ACTION_VIEW)
+                        .setDataAndType(uriForFile, mime);
+                
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                // Set flag to give temporary permission to external app to use FileProvider
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                // Validate that the device can open the file
+                PackageManager pm = getCurrentActivity().getPackageManager();
+                if (intent.resolveActivity(pm) != null) {
+                    this.getReactApplicationContext().startActivity(intent);
+                }
+
+            } else {
+                Intent intent = new Intent(Intent.ACTION_VIEW)
+                        .setDataAndType(Uri.parse("file://" + path), mime).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                this.getReactApplicationContext().startActivity(intent);
+            }
+            ActionViewVisible = true;
+
+            final LifecycleEventListener listener = new LifecycleEventListener() {
+                @Override
+                public void onHostResume() {
+                    if(ActionViewVisible)
+                        promise.resolve(null);
+                    this.reactContext.removeLifecycleEventListener(this);
+                }
+
+                @Override
+                public void onHostPause() {
+
+                }
+
+                @Override
+                public void onHostDestroy() {
+
+                }
+            };
+            this.reactContext.addLifecycleEventListener(listener);
+        } catch(Exception ex) {
+            promise.reject("EUNSPECIFIED", ex.getLocalizedMessage());
+        }
+    }
 
   @ReactMethod
   public void reloadJSBundle() {
